e7613bc6d10ab6109d889b3f2420c23979904fe5
Miike Ramos <mikeramos@gmail.com>
2013-05-13 23:26:45 -0700

    Added date and finished core requirements

--- twittler.js
+++ twittler.js
@@ -1,5 +1,62 @@
 $(document).ready(function(){
   var globalUser = '';
+
+  var formatDate = function(dateSource) {
+    var month = dateSource.getMonth() + 1;
+    var day = dateSource.getDate();
+    var hours = dateSource.getHours();
+    var minutes = dateSource.getMinutes() < 10 ? '0' + dateSource.getMinutes() : dateSource.getMinutes();
+    var timePeriod = '';
+    
+    switch (month) {
+      case 0:
+        month = "Jan";
+        break;
+      case 1:
+        month = "Feb";
+        break;
+      case 2:
+        month = "Mar";
+        break;
+      case 3:
+        month = "Apr";
+        break;
+      case 4:
+        month = "May";
+        break;
+      case 5:
+        month = "Jun";
+        break;
+      case 6:
+        month = "Jul";
+        break;
+      case 7:
+        month = "Aug";
+        break;
+      case 8:
+        month = "Sep";
+        break;
+      case 9:
+        month = "Oct";
+        break;
+      case 10:
+        month = "Nov";
+        break;
+      case 11:
+        month = "Dec";
+        break;
+    }
+
+    if (hours > 12) {
+      hours -= 12;
+      timePeriod = "pm";
+    } else {
+      timePeriod = "am";
+    }
+
+    return month + ' ' + day + ', ' + hours + ':' + minutes + timePeriod;
+  };
+
   var grabTweets = function (user) {
     var $body = $('.tweets');
     $body.html('');
@@ -9,7 +66,7 @@ $(document).ready(function(){
       while(index >= 0){
         var tweet = streams.home[index];
         var $tweet = $('<div class="tweet"></div>');
-        $tweet.html('<a href="#" class="' + tweet.user + '">@' + tweet.user + '</a>: ' + tweet.message);// + ' (' + tweet.created_at + ')');
+        $tweet.html('<a href="#" class="' + tweet.user + '">@' + tweet.user + '</a>: ' + tweet.message + ' <em>' + formatDate(tweet.created_at) + '</em>');
         $tweet.appendTo($body);
         index -= 1;
       }
